57015738fb446c4d85b25fa0c1d91abd7aaf4a74
YoussefDarwishh <youssefdarwishh@gmail.com>
2023-07-10 11:49:04 +0300

    Removed useless method and used file scoped namespace

--- RenderOPML/Pages/Index.cshtml.cs
+++ RenderOPML/Pages/Index.cshtml.cs
@@ -6,101 +6,94 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using System.Xml;
 
-namespace RenderOPML.Pages
+namespace RenderOPML.Pages;
+public class IndexModel : PageModel
 {
-    public class IndexModel : PageModel
+    private readonly IHttpClientFactory _httpClientFactory;
+    public List<FeedItemOpml> FeedItems { get; set; } = new List<FeedItemOpml>();
+
+    public int PageSize { get; } = 5;
+    public int CurrentPage { get; set; } = 1;
+    public int TotalPages { get; set; } = 1;
+
+    public IndexModel(IHttpClientFactory httpClientFactory)
     {
-        private readonly IHttpClientFactory _httpClientFactory;
-        public List<FeedItemOpml> FeedItems { get; set; } = new List<FeedItemOpml>();
+        _httpClientFactory = httpClientFactory;
+    }
 
-        public int PageSize { get; } = 5;
-        public int CurrentPage { get; set; } = 1;
-        public int TotalPages { get; set; } = 1;
+    public async Task<IActionResult> OnGetAsync(int currentPage = 1)
+    {
+        var httpClient = _httpClientFactory.CreateClient();
+        var response = await httpClient.GetAsync("https://blue.feedland.org/opml?screenname=dave");
 
-        public IndexModel(IHttpClientFactory httpClientFactory)
+        if (response.IsSuccessStatusCode)
         {
-            _httpClientFactory = httpClientFactory;
+            var xmlContent = await response.Content.ReadAsStringAsync();
+            var allFeedItems = ParseOpmlContent(xmlContent);
+
+            var startIndex = (currentPage - 1) * PageSize;
+            var endIndex = Math.Min(startIndex + PageSize, allFeedItems.Count);
+            FeedItems = allFeedItems.GetRange(startIndex, endIndex - startIndex);
+
+            TotalPages = (int)Math.Ceiling((double)allFeedItems.Count / PageSize);
+
+            CurrentPage = Math.Max(1, Math.Min(currentPage, TotalPages));
+
+            return Page();
         }
-
-        public async Task<IActionResult> OnGetAsync(int currentPage = 1)
+        else
         {
-            var httpClient = _httpClientFactory.CreateClient();
-            var response = await FetchXmlContentAsync(httpClient, "https://blue.feedland.org/opml?screenname=dave");
-
-            if (response.IsSuccessStatusCode)
-            {
-                var xmlContent = await response.Content.ReadAsStringAsync();
-                var allFeedItems = ParseOpmlContent(xmlContent);
-
-                var startIndex = (currentPage - 1) * PageSize;
-                var endIndex = Math.Min(startIndex + PageSize, allFeedItems.Count);
-                FeedItems = allFeedItems.GetRange(startIndex, endIndex - startIndex);
-
-                TotalPages = (int)Math.Ceiling((double)allFeedItems.Count / PageSize);
-
-                CurrentPage = Math.Max(1, Math.Min(currentPage, TotalPages));
-
-                return Page();
-            }
-            else
-            {
-                return RedirectToPage("/Error");
-            }
+            return RedirectToPage("/Error");
         }
+    }
 
-        async Task<HttpResponseMessage> FetchXmlContentAsync(HttpClient httpClient, string url)
+    List<FeedItemOpml> ParseOpmlContent(string opmlContent)
+    {
+        var feedItems = new List<FeedItemOpml>();
+        XmlDocument doc = new XmlDocument();
+        doc.LoadXml(opmlContent);
+
+        XmlNodeList outlineNodes = doc.GetElementsByTagName("outline");
+
+        foreach (XmlNode outlineNode in outlineNodes)
         {
-            return await httpClient.GetAsync(url);
-        }
+            string xmlUrl = outlineNode.Attributes["xmlUrl"]?.Value;
+            string text = outlineNode.Attributes["text"]?.Value;
 
-        List<FeedItemOpml> ParseOpmlContent(string opmlContent)
-        {
-            var feedItems = new List<FeedItemOpml>();
-            XmlDocument doc = new XmlDocument();
-            doc.LoadXml(opmlContent);
-
-            XmlNodeList outlineNodes = doc.GetElementsByTagName("outline");
-
-            foreach (XmlNode outlineNode in outlineNodes)
+            if (!string.IsNullOrEmpty(xmlUrl) && !string.IsNullOrEmpty(text))
             {
-                string xmlUrl = outlineNode.Attributes["xmlUrl"]?.Value;
-                string text = outlineNode.Attributes["text"]?.Value;
-
-                if (!string.IsNullOrEmpty(xmlUrl) && !string.IsNullOrEmpty(text))
+                FeedItemOpml feedItem = new FeedItemOpml
                 {
-                    FeedItemOpml feedItem = new FeedItemOpml
-                    {
-                        XmlUrl = xmlUrl,
-                        Text = text
-                    };
+                    XmlUrl = xmlUrl,
+                    Text = text
+                };
 
-                    feedItems.Add(feedItem);
-                }
+                feedItems.Add(feedItem);
             }
-
-            return feedItems;
         }
 
-        public IActionResult OnGetRenderXml(string xmlUrl)
+        return feedItems;
+    }
+
+    public IActionResult OnGetRenderXml(string xmlUrl)
+    {
+        var httpClient = _httpClientFactory.CreateClient();
+        var response = httpClient.GetAsync(xmlUrl).Result;
+
+        if (response.IsSuccessStatusCode)
         {
-            var httpClient = _httpClientFactory.CreateClient();
-            var response = httpClient.GetAsync(xmlUrl).Result;
-
-            if (response.IsSuccessStatusCode)
-            {
-                var xmlContent = response.Content.ReadAsStringAsync().Result;
-                return Content(xmlContent, "text/xml");
-            }
-            else
-            {
-                return Content("Failed to retrieve XML content.");
-            }
+            var xmlContent = response.Content.ReadAsStringAsync().Result;
+            return Content(xmlContent, "text/xml");
         }
-
-        public class FeedItemOpml
+        else
         {
-            public string XmlUrl { get; set; }
-            public string Text { get; set; }
+            return Content("Failed to retrieve XML content.");
         }
+    }
+
+    public class FeedItemOpml
+    {
+        public string XmlUrl { get; set; }
+        public string Text { get; set; }
     }
 }
--- RenderOPML/Pages/RenderXML.cshtml.cs
+++ RenderOPML/Pages/RenderXML.cshtml.cs
@@ -1,77 +1,72 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
 using System.Xml;
 
-namespace RenderOPML.Pages
+namespace RenderOPML.Pages;
+
+public class RenderXMLModel : PageModel
 {
-    public class RenderXMLModel : PageModel
+    private readonly IHttpClientFactory _httpClientFactory;
+    public List<FeedItemXml> FeedItems { get; set; } = new List<FeedItemXml>();
+
+    public RenderXMLModel(IHttpClientFactory httpClientFactory)
     {
-        private readonly IHttpClientFactory _httpClientFactory;
-        public List<FeedItemXml> FeedItems { get; set; } = new List<FeedItemXml>();
+        _httpClientFactory = httpClientFactory;
+    }
 
-        public RenderXMLModel(IHttpClientFactory httpClientFactory)
+    public async Task<IActionResult> OnGetAsync(string xmlUrl)
+    {
+        if (string.IsNullOrEmpty(xmlUrl))
         {
-            _httpClientFactory = httpClientFactory;
+            return RedirectToPage("/Error");
         }
 
-        public async Task<IActionResult> OnGetAsync(string xmlUrl)
+        var httpClient = _httpClientFactory.CreateClient();
+        var response = await httpClient.GetAsync(xmlUrl);
+
+        if (response.IsSuccessStatusCode)
         {
-            if (string.IsNullOrEmpty(xmlUrl))
-            {
-                return RedirectToPage("/Error");
-            }
+            var xmlContent = await response.Content.ReadAsStringAsync();
+            FeedItems = ParseXmlContent(xmlContent);
 
-            var httpClient = _httpClientFactory.CreateClient();
-            var response = await FetchXmlContentAsync(httpClient, xmlUrl);
-
-            if (response.IsSuccessStatusCode)
-            {
-                var xmlContent = await response.Content.ReadAsStringAsync();
-                FeedItems = ParseXmlContent(xmlContent);
-
-                return Page();
-            }
-            else
-            {
-                return RedirectToPage("/Error");
-            }
+            return Page();
         }
-
-        async Task<HttpResponseMessage> FetchXmlContentAsync(HttpClient httpClient, string url)
+        else
         {
-            return await httpClient.GetAsync(url);
-        }
-
-        List<FeedItemXml> ParseXmlContent(string xmlContent)
-        {
-            var feedItems = new List<FeedItemXml>();
-            XmlDocument doc = new XmlDocument();
-            doc.LoadXml(xmlContent);
-
-            XmlNodeList channelNodes = doc.GetElementsByTagName("channel");
-            XmlNodeList itemNodes = channelNodes[0].SelectNodes("item");
-
-            foreach (XmlNode itemNode in itemNodes)
-            {
-                FeedItemXml feedItem = new FeedItemXml();
-
-                feedItem.Title = itemNode.SelectSingleNode("title")?.InnerText ?? string.Empty;
-                feedItem.Description = itemNode.SelectSingleNode("description")?.InnerText ?? string.Empty;
-                feedItem.PubDate = DateTime.Parse(itemNode.SelectSingleNode("pubDate")?.InnerText ?? string.Empty);
-                feedItem.Link = itemNode.SelectSingleNode("link")?.InnerText ?? string.Empty;
-
-                feedItems.Add(feedItem);
-            }
-
-            return feedItems;
+            return RedirectToPage("/Error");
         }
     }
 
-    public class FeedItemXml
+    List<FeedItemXml> ParseXmlContent(string xmlContent)
     {
-        public string? Title { get; set; }
-        public string Description { get; set; }
-        public DateTime PubDate { get; set; }
-        public string Link { get; set; }
+        var feedItems = new List<FeedItemXml>();
+        XmlDocument doc = new XmlDocument();
+        doc.LoadXml(xmlContent);
+
+        XmlNodeList channelNodes = doc.GetElementsByTagName("channel");
+        XmlNodeList itemNodes = channelNodes[0].SelectNodes("item");
+
+        foreach (XmlNode itemNode in itemNodes)
+        {
+            FeedItemXml feedItem = new FeedItemXml();
+
+            feedItem.Title = itemNode.SelectSingleNode("title")?.InnerText ?? string.Empty;
+            feedItem.Description = itemNode.SelectSingleNode("description")?.InnerText ?? string.Empty;
+            feedItem.PubDate = DateTime.Parse(itemNode.SelectSingleNode("pubDate")?.InnerText ?? string.Empty);
+            feedItem.Link = itemNode.SelectSingleNode("link")?.InnerText ?? string.Empty;
+
+            feedItems.Add(feedItem);
+        }
+
+        return feedItems;
     }
+}
+
+public class FeedItemXml
+{
+    public string? Title { get; set; }
+    public string Description { get; set; }
+    public DateTime PubDate { get; set; }
+    public string Link { get; set; }
 }
